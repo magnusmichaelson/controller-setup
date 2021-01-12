a8b1c582a80fad7dd5f61195248e92e4fe95c647
magnus <magnusmichaelson@protonmail.com>
2021-01-12 23:57:39 +0000

    same function works for mac and linux now

--- core.js
+++ core.js
@@ -1,50 +1,53 @@
-var controllerActive = false;
 // @ts-ignore
 var rAF = window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.requestAnimationFrame;
-// @ts-ignore
-var rAFStop = window.mozCancelRequestAnimationFrame || window.webkitCancelRequestAnimationFrame || window.cancelRequestAnimationFrame;
-window.addEventListener("gamepadconnected", function () {
-    controllerActive = true;
-});
-window.addEventListener("gamepaddisconnected", function () {
-    controllerActive = false;
-});
 gameLoop();
 function gameLoop() {
-    if (controllerActive) {
-        gamepadStart();
-    }
+    gamepadStart();
     var start = rAF(gameLoop);
 }
 ;
 function gamepadStart() {
     if (navigator.platform == "MacIntel") {
-        controllerMac(0);
+        //controllerMac(0);
+        controllerLinuxOrMac();
+    }
+    if (navigator.platform == "Linux x86_64") {
+        controllerLinuxOrMac();
     }
 }
-function controllerMac(delta) {
+function controllerLinuxOrMac() {
     var buttons = {};
     var controller;
     var controllerData;
-    controller = navigator.getGamepads()[0];
-    if ("id" in controller && controller["id"] == "Xbox 360 Controller (STANDARD GAMEPAD Vendor: 045e Product: 028e)") {
-        if ("axes" in controller && "buttons" in controller) {
-            buttons = controller["buttons"];
-            if (controller["axes"].length > 3) {
-                if (4 in buttons && 5 in buttons && 6 in buttons && 7 in buttons) {
-                    controllerData = {
-                        clickLeft: controller.buttons[4].value,
-                        clickRight: controller.buttons[5].value,
-                        down: controller.buttons[6].value,
-                        leftStickHorizontal: controller.axes[0],
-                        leftStickVertical: controller.axes[1],
-                        rightStickHorizontal: controller.axes[2],
-                        rightStickVertical: controller.axes[3],
-                        up: controller.buttons[7].value
-                    };
-                    controllerAction(controllerData);
+    // @ts-ignore
+    controller = navigator.getGamepads();
+    if (controller) {
+        var output = document.getElementById("test");
+        if (output) {
+            Object.keys(controller).forEach(function (key) {
+                if (controller[key]) {
+                    if ("id" in controller[key] && controller[key]["id"].includes("(STANDARD GAMEPAD Vendor: 045e Product: 028e)")) {
+                        if ("axes" in controller[key] && "buttons" in controller[key]) {
+                            buttons = controller[key]["buttons"];
+                            if (controller[key]["axes"].length > 3) {
+                                if (4 in buttons && 5 in buttons && 6 in buttons && 7 in buttons) {
+                                    controllerData = {
+                                        clickLeft: controller[key].buttons[4].value,
+                                        clickRight: controller[key].buttons[5].value,
+                                        down: controller[key].buttons[6].value,
+                                        leftStickHorizontal: controller[key].axes[0],
+                                        leftStickVertical: controller[key].axes[1],
+                                        rightStickHorizontal: controller[key].axes[2],
+                                        rightStickVertical: controller[key].axes[3],
+                                        up: controller[key].buttons[7].value
+                                    };
+                                    controllerAction(controllerData);
+                                }
+                            }
+                        }
+                    }
                 }
-            }
+            });
         }
     }
 }
@@ -53,17 +56,4 @@ function controllerAction(controllerData) {
     if (output) {
         output.innerHTML = JSON.stringify(controllerData, null, 4);
     }
-}
-function gamePadExtra(gamePad) {
-    var output = document.getElementById("test");
-    for (var axisCount = 0; axisCount < gamePad["axes"].length; axisCount++) {
-        if (output) {
-            output.innerHTML += "axis " + axisCount + " " + gamePad["axes"][axisCount] + "<br/>";
-        }
-    }
-    Object.keys(gamePad["buttons"]).forEach(function (buttonKey) {
-        if (output) {
-            output.innerHTML += "button " + buttonKey + " " + JSON.stringify(gamePad["buttons"][buttonKey]["value"]) + "<br/>";
-        }
-    });
 }
--- core.ts
+++ core.ts
@@ -8,60 +8,62 @@ interface ControllerNavigate {
     rightStickVertical: number;
     up: number;
 }
-
-var controllerActive = false;
 // @ts-ignore
 var rAF = window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.requestAnimationFrame;
-// @ts-ignore
-var rAFStop = window.mozCancelRequestAnimationFrame || window.webkitCancelRequestAnimationFrame || window.cancelRequestAnimationFrame;
-window.addEventListener("gamepadconnected", function() {
-    controllerActive = true;
-});
-window.addEventListener("gamepaddisconnected", function() {
-    controllerActive = false;
-});
-
 gameLoop();
 
 function gameLoop() {
-    if (controllerActive){
-        gamepadStart();
-    }
+    gamepadStart();
     var start = rAF(gameLoop);
 };
 
 function gamepadStart(){
     if (navigator.platform == "MacIntel"){
-        controllerMac(0);
+        //controllerMac(0);
+        controllerLinuxOrMac();
+    }
+    if (navigator.platform == "Linux x86_64"){
+        controllerLinuxOrMac();
     }
 }
 
-function controllerMac(delta: number){
+function controllerLinuxOrMac(){
     var buttons: Record<string,number> = {};
     var controller: any;
     var controllerData: ControllerNavigate;
-    controller = navigator.getGamepads()[0];
-    if  ("id" in controller && controller["id"] == "Xbox 360 Controller (STANDARD GAMEPAD Vendor: 045e Product: 028e)"){
-        if ("axes" in controller && "buttons" in controller){
-            buttons = controller["buttons"];
-            if (controller["axes"].length > 3){
-                if (4 in buttons && 5 in buttons && 6 in buttons && 7 in buttons){
-                    controllerData = {
-                        clickLeft: controller.buttons[4].value,
-                        clickRight: controller.buttons[5].value,
-                        down: controller.buttons[6].value,
-                        leftStickHorizontal: controller.axes[0],
-                        leftStickVertical: controller.axes[1],
-                        rightStickHorizontal: controller.axes[2],
-                        rightStickVertical: controller.axes[3],
-                        up: controller.buttons[7].value
+    // @ts-ignore
+    controller = navigator.getGamepads();
+    if (controller){
+        var output = document.getElementById("test");
+        if (output){
+            Object.keys(controller).forEach(function(key){
+                if (controller[key]){
+                    if  ("id" in controller[key] && controller[key]["id"].includes("(STANDARD GAMEPAD Vendor: 045e Product: 028e)")){
+                        if ("axes" in controller[key] && "buttons" in controller[key]){
+                            buttons = controller[key]["buttons"];
+                            if (controller[key]["axes"].length > 3){
+                                if (4 in buttons && 5 in buttons && 6 in buttons && 7 in buttons){
+                                    controllerData = {
+                                        clickLeft: controller[key].buttons[4].value,
+                                        clickRight: controller[key].buttons[5].value,
+                                        down: controller[key].buttons[6].value,
+                                        leftStickHorizontal: controller[key].axes[0],
+                                        leftStickVertical: controller[key].axes[1],
+                                        rightStickHorizontal: controller[key].axes[2],
+                                        rightStickVertical: controller[key].axes[3],
+                                        up: controller[key].buttons[7].value
+                                    }
+                                    controllerAction(controllerData);
+                                }
+                            }
+                        } 
                     }
-                    controllerAction(controllerData);
                 }
-            }
-        } 
+            })
+        }
     }
 }
+
 
 function controllerAction(controllerData: ControllerNavigate){
     var output = document.getElementById("test");
